6ce09c2a4248a5a3cd648a3a35e0d749cc7e32c8
Jobin Johnson <jobinrjohnson@gmail.com>
2018-08-22 12:16:59 +0530

    Say thanks and disable functionality
    Signed-off-by: Jobin Johnson <jobinrjohnson@gmail.com>

--- app/src/main/java/in/keralafloodrelief/Thanks.java
+++ app/src/main/java/in/keralafloodrelief/Thanks.java
@@ -1,13 +1,11 @@
 package in.keralafloodrelief;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Splash extends AppCompatActivity {
+public class Thanks extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +13,8 @@ public class Splash extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_thanks);
         getSupportActionBar().hide();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(Splash.this, Thanks.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
-        }, 1000);
 
     }
 }
